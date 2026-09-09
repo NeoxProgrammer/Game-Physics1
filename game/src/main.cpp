bb54c5625876e93227581b10f4a8ac5b72085d22
BashaarAli <bashaar08@gmail.com>
2026-09-09 17:13:19 -0400

    Finsihed lab practise 1(not to be marked lol)

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,14 +7,32 @@ int main()
     InitWindow(800, 800, "Physics-1");
     InitAudioDevice();
     SetTargetFPS(60);
+    Vector2 RandomPos = { GetRandomValue(0, 800), GetRandomValue(0, 800) };
+
+	float x = 400, y = 400;
+    float a = 6.0f; // amplitude radius of movement how fast it goes around the circle
+    float b = 5.1f; // frequency of movement kind of how far it goes around the circle when changing the values thats what i found lol
+    
+    
 
     while (!WindowShouldClose())
     {
+		float t = GetTime();
+		float dt = GetFrameTime();
+
+		y = y + (cos(t * a)) * a * b * dt; // Update y position based on cosine function
+		x = x + (-sin(t * a)) * a * b * dt; // Update x position based on sine function
+		
+
+		float time = GetTime();
         BeginDrawing();
         ClearBackground(WHITE);
 
-        DrawCircleV(GetMousePosition(), 20.0f, RED);
-		DrawCircleV(GetMousePosition(), 40.0f, BLUE);
+        DrawText("Bashaar Ali 101545977", 50, 750, 30, RED);
+        
+        const char* text = TextFormat("Time: %f", time); 
+        DrawText(text, 400, 400, 20, GREEN); 
+        DrawCircle(x,y, 20,DARKPURPLE ); 
 
         EndDrawing();
     }
